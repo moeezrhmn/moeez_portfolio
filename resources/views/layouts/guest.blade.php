8c48e39c56ff97276017de75f4469af858f58cc1
baabaylog <baabaylog@gmail.com>
2024-10-22 07:59:05 +0500

    .

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title> {{ config('app.name', 'Moeez dev') }} @hasSection ('title') | @yield('title') @endif </title>
-    <meta name="msapplication-TileImage" content="{{ asset(assets/images/moeez-dp-2.png) }}">
+    <meta name="msapplication-TileImage" content="{{ asset('assets/images/moeez-dp-2.png') }}">
 
     {{-- Boostrap 5.3v --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
