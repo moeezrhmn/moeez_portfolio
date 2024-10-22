5d132a9a100b6fa27a568eb0e396d2a240f7f75e
baabaylog <baabaylog@gmail.com>
2024-10-22 08:17:11 +0500

    add meta tags for social meda and for SEO

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,8 +5,29 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title> {{ config('app.name', 'Moeez dev') }} @hasSection ('title') | @yield('title') @endif </title>
+    <title> {{ config('app.name', 'Moeez Dev | Web Apps, Automation, SaaS, Inventory & Custom Solutions') }} @hasSection ('title') | @yield('title') @endif </title>
+
+    <!-- Primary Meta Tags -->
+    <meta name="title" content="Moeez Dev | Web Apps, Automation, SaaS, Inventory & Custom Solutions">
+    <meta name="description" content="Moeez, a skilled software developer specializing in web apps, automation, SaaS products, web scraping, and inventory management solutions. Expertise in PHP (Laravel, CodeIgniter), JavaScript (Node.js, ReactJS), Python (Flask, FastAPI), and WordPress plugin development. Build user-friendly websites, automate tasks, integrate APIs, and create custom SaaS applications.">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://moeezrehman.dev/">
+    <meta property="og:title" content="Moeez Dev | Web Apps, Automation, SaaS, Inventory & Custom Solutions">
+    <meta property="og:description" content="Professional web apps, SaaS development, automation, inventory management, and custom solutions. Build user-friendly websites, automate tasks, and integrate APIs with expertise in Laravel, Python, and JavaScript.">
+    <meta property="og:image" content="{{ asset('assets/images/moeez-dp-2.png') }}">
+    
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://moeezrehman.dev/">
+    <meta property="twitter:title" content="Moeez Dev | Web Apps, Automation, SaaS, Inventory & Custom Solutions">
+    <meta property="twitter:description" content="Expert in custom software solutions, web apps, automation, SaaS products, and API integrations. Build efficient, scalable systems for various industries.">
+    <meta property="twitter:image" content="{{ asset('assets/images/moeez-dp-2.png') }}">
+    
+    
     <meta name="msapplication-TileImage" content="{{ asset('assets/images/moeez-dp-2.png') }}">
+
 
     {{-- Boostrap 5.3v --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
