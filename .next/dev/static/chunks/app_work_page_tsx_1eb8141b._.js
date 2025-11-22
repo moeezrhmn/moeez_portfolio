795c3e2694a14481fe425f9aa0a380fb24ae197d
moeezrhmn <baabaylog@gmail.com>
2025-11-23 00:06:32 +0500

    build added

--- .next/dev/static/chunks/app_work_page_tsx_1eb8141b._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_work_page_tsx_73e96048._.js"
-],
-    source: "dynamic"
-});
